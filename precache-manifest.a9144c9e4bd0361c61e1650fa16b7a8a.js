a13dd8ae0728b6af5300e8fdd92c9255ff62d0ab
unknown <artak.chatilyan@gmail.com>
2020-08-16 18:58:01 +0400

    Updates

---
+++ precache-manifest.a9144c9e4bd0361c61e1650fa16b7a8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9b0c2db4f39744c429407373a78909c",
+    "url": "/Shopping/index.html"
+  },
+  {
+    "revision": "50fdbb28200c8510b067",
+    "url": "/Shopping/static/css/main.207b42a4.chunk.css"
+  },
+  {
+    "revision": "8b930427a48772e862b9",
+    "url": "/Shopping/static/js/2.4ca4cdd5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Shopping/static/js/2.4ca4cdd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50fdbb28200c8510b067",
+    "url": "/Shopping/static/js/main.77a2961e.chunk.js"
+  },
+  {
+    "revision": "caa8901197810a602505",
+    "url": "/Shopping/static/js/runtime-main.325e225b.js"
+  },
+  {
+    "revision": "3c426dd837b245261cf855a02ff182f9",
+    "url": "/Shopping/static/media/logo_dark.3c426dd8.png"
+  }
+]);
